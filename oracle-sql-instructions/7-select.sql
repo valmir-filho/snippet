6c695a668e9153f93cda572069ce5d107b55cc42
Valmir <vmcfilho@gmail.com>
2024-04-22 15:25:36 -0300

    Update 7-select.sql

--- oracle-sql-instructions/7-select.sql
+++ oracle-sql-instructions/7-select.sql
@@ -1,7 +1,13 @@
 /*
-The SQL command SELECT * FROM employees; is used to retrieve all records
-from the employees table in a database. This command selects every column
-and every row from the table, giving you a complete view of all the data
-stored in employees.
+The SQL command is used to retrieve all records from the employees table
+in a database. This command selects every column and every row from the
+table, giving you a complete view of all the data stored in employees.
 */
 SELECT * FROM employees;
+
+/*
+The SQL command retrieves two specific columns, department_id and location_id,
+from the departments table in a database.
+*/
+SELECT department_id, location_id FROM departments;
+
