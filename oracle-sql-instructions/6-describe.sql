ae473316100a851419ff82e05d2ff1ad0da8af4c
Valmir <vmcfilho@gmail.com>
2024-04-22 15:16:54 -0300

    Update 6-describe.sql

--- oracle-sql-instructions/6-describe.sql
+++ oracle-sql-instructions/6-describe.sql
@@ -1,3 +1,3 @@
--- Command seems like a SQL statement used to display the structure of the employees table in a database. --
+-- A SQL statement used to display the structure of the employees table in a database. --
 DESCRIBE employees;
 -- DESC employees; --
