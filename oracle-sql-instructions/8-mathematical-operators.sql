7a65dd12c529a22e189773e2c2680d8aa3365600
Valmir <vmcfilho@gmail.com>
2024-04-22 15:36:55 -0300

    Create 8-mathematical-operators.sql

---
+++ oracle-sql-instructions/8-mathematical-operators.sql
@@ -0,0 +1,12 @@
+/*
+The SQL command is structured to select specific columns from
+the employees table and also compute a new value based on one
+of these columns.
+*/
+
+SELECT
+    first_name,
+    last_name,
+    salary, salary * 1.15
+FROM
+    employees;
