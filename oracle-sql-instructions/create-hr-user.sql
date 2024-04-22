c3c3e47e0c1b7d8c471194e6238e63626f644c7b
Valmir <vmcfilho@gmail.com>
2024-04-22 12:05:09 -0300

    Update create-hr-user.sql

--- oracle-sql-instructions/create-hr-user.sql
+++ oracle-sql-instructions/create-hr-user.sql
@@ -1,29 +1,30 @@
--- Change the current session's context to the pluggable database XEPDB1. --
+-- Switch the current session to the pluggable database "XEPDB1". --
 ALTER SESSION SET CONTAINER = XEPDB1;
 
--- Drop the user HR along with all of its associated objects recursively. --
+/*
+  Drop the user "HR" and all associated objects recursively.
+  This is useful for cleaning up before re-creating a user setup.
+*/
 DROP USER HR CASCADE;
 
--- Create a new user named HR with the password "hr". --
-CREATE USER HR IDENTIFIED BY hr;
-
--- Set the default tablespace for the user HR to "users". --
-DEFAULT TABLESPACE users;
-
--- Set the temporary tablespace for the user HR to "TEMP". --
-TEMPORARY TABLESPACE TEMP;
-
--- Grant unlimited quota on the "users" tablespace to the user HR. --
-QUOTA unlimite on users;
-
--- Set the quota on the "system" tablespace to 0 for the user HR. --
+/*
+  Create a new user named "HR" with the specified password "hr".
+  Set the default tablespace to "users" and temporary tablespace to "TEMP".
+  Assign unlimited quota on the "users" tablespace and no quota on "system" tablespace.
+  This setup is common for development environments where the user needs flexibility.
+*/
+CREATE USER HR 
+IDENTIFIED BY hr
+DEFAULT TABLESPACE users
+TEMPORARY TABLESPACE TEMP
+QUOTA unlimited on users
 QUOTA 0 on system;
 
--- Grant the CONNECT and RESOURCE roles to the user HR. --
+-- Grant basic connection and resource management roles to "HR". --
 GRANT CONNECT, RESOURCE TO HR;
 
--- Grant permissions to create sessions, views, tables, modify sessions, sequences, procedures, and triggers to the user HR. --
+-- Grant permissions to create and manage sessions, views, tables, sequences, procedures, and triggers. --
 GRANT CREATE SESSION, CREATE VIEW, CREATE TABLE, ALTER SESSION, CREATE SEQUENCE, CREATE PROCEDURE, CREATE TRIGGER TO HR;
 
--- Grant permissions to create synonyms, database links, and use unlimited tablespace to the user HR. --
+-- Additionally grant permissions to create synonyms, database links, and unlimited usage of tablespaces. --
 GRANT CREATE SYNONYM, CREATE DATABASE LINK, UNLIMITED TABLESPACE TO HR;
