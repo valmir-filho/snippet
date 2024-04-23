f97460aed18ac217413efe5c4f88f6cc70ecd027
Valmir <vmcfilho@gmail.com>
2024-04-23 10:31:08 -0300

    Create 10-column-alias.sql

---
+++ oracle-sql-instructions/10-column-alias.sql
@@ -0,0 +1,12 @@
+/*
+The SQL command you've provided selects specific columns
+from the employees table, with some of the columns being
+renamed using aliases for clarity or presentation in the results.
+*/
+
+SELECT
+    first_name AS Name,
+    last_name Sobrenome,
+    salary AS Salário
+FROM
+    employees;
