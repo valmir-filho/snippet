4f0608e24eaca5ae2be0a948efca996a71ca179e
Valmir <vmcfilho@gmail.com>
2024-04-23 10:31:43 -0300

    Update 10-column-alias.sql

--- oracle-sql-instructions/10-column-alias.sql
+++ oracle-sql-instructions/10-column-alias.sql
@@ -3,7 +3,6 @@ The SQL command you've provided selects specific columns
 from the employees table, with some of the columns being
 renamed using aliases for clarity or presentation in the results.
 */
-
 SELECT
     first_name AS Name,
     last_name Sobrenome,
