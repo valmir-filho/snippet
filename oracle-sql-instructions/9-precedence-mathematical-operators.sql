68cf6da8ba2181683ad1734d88cea52dab688133
Valmir <vmcfilho@gmail.com>
2024-04-23 10:23:49 -0300

    Update 9-precedence-mathematical-operators.sql

--- oracle-sql-instructions/9-precedence-mathematical-operators.sql
+++ oracle-sql-instructions/9-precedence-mathematical-operators.sql
@@ -1,2 +1,13 @@
+/*
+This SQL command select specific columns
+from the employees table and also compute
+an increment to the salary. 
+*/
 
-In the SQL command you've provided, the intention appears to be to select specific columns from the employees table and also compute an increment to the salary. 
+SELECT
+    first_name,
+    last_name,
+    salary,
+    salary + 100 * 1.15
+FROM
+    employees;
