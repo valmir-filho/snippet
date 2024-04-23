05be91959ffe17a122281e2f24d070b0f48c889e
Valmir <vmcfilho@gmail.com>
2024-04-23 12:30:10 -0300

    Delete oracle-sql-instructions directory

--- oracle-sql-instructions/01-obtain-current-system-date-and-time.sql
+++
@@ -1,5 +0,0 @@
--- Used in Oracle databases to obtain the current system date and time. --
-SELECT
-    sysdate "SYSDATE"
-FROM
-    "SYS"."DUAL" "A1";
--- oracle-sql-instructions/06-describe.sql
+++
@@ -1,3 +0,0 @@
--- A SQL statement used to display the structure of the employees table in a database. --
-DESCRIBE employees;
--- DESC employees; --
